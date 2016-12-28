db33bbc8bfe7012d7c6bfdbb983d8ebef5f3c1b4
metigel94 <leon-mueller@hotmail.de>
2016-12-28 12:37:46 +0100

    Implementet BubbleSort Algorithm - int Array only
    Implemented the BubbleSort Algorithm, but at the moment only for an
    Integer Array.

--- MiniProject/MiniProject/BubbleSort.cs
+++ MiniProject/MiniProject/BubbleSort.cs
@@ -4,9 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MiniProject
+namespace BubbleSortSearch
 {
-    class BubbleSort
+    class Program
     {
+        public static int[] BubbleSort(int[] array)
+        {
+            int length = array.Length;
+
+            int temp = array[0];
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i+1; j < length; j++)
+                {
+                    if(array[i] > array[j])
+                    {
+                        temp = array[i];
+
+                        array[i] = array[j];
+
+                        array[j] = temp;
+                    }
+
+                }
+
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {            
+        }
     }
 }
